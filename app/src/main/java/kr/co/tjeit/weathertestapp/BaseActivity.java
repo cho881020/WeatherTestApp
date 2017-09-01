5bdd62e857ec7de6c6cd82be3c1f2f95788fd602
cho881020 <cho881020@gmail.com>
2017-09-01 12:03:12 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjeit/weathertestapp/BaseActivity.java
@@ -0,0 +1,16 @@
+package kr.co.tjeit.weathertestapp;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by user on 2017-09-01.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+    Context mContext = this;
+
+    public abstract void setupEvents();
+    public abstract void setValues();
+    public abstract void bindViews  ();
+}
